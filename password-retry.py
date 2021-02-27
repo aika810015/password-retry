d0c6f8557ac1fee75ee897b75b3d703d124a0490
Rance SHIH <aika810015jp@gmail.com>
2021-02-27 14:15:29 +0900

    finish

--- password-retry.py
+++ password-retry.py
@@ -1,11 +1,15 @@
 password = "a123456"
 i = 3
 while i > 0:
+	i = i - 1
 	pwd = input("パスワードを入力してください：")
 	if pwd == password:
 		print("ログイン成功")
 		break
 	else:
-		i = i - 1
-		print("パスワード間違いました、あと", i,"回")
+		print("パスワード間違いました！")
+		if i > 0:
+			print("あと", i, "回です")
+		else:
+			print("パスワード間違いました、アカウントロックしました。")
 
